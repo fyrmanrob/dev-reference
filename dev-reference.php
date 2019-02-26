3e2e40d9aafb4c3488a2d529f8a11b34ea5e5667
Rob McClara <robmcclara@gmail.com>
2019-02-26 07:38:09 -0500

    Update dev-reference.php

--- dev-reference.php
+++ dev-reference.php
@@ -7,7 +7,6 @@
  *	Version: 1.0
  *	Author: Rob McClara
  *	Author URI: http://fyrmanrob.net
- *	GitHub Plugin URI: fyrmanrob/dev-reference
  *	GitHub Plugin URI: https://github.com/fyrmanrob/dev-reference
  */
 
